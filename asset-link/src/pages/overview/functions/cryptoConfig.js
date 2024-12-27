308825ce3e4f8ae8fe85aaeb8c0a5e9a6a58d457
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-12-27 15:19:22 +0100

    Add more coins

--- asset-link/src/pages/overview/functions/cryptoConfig.js
+++ asset-link/src/pages/overview/functions/cryptoConfig.js
@@ -4,5 +4,10 @@ export const supportedCryptos = [
     { symbol: 'USDT', name: 'Tether', id: 'tether' },
     { symbol: 'XRP', name: 'Ripple', id: 'ripple' },
     { symbol: 'SOL', name: 'Solana', id: 'solana' },
-    { symbol: 'DOGE', name: 'Dogecoin', id: 'dogecoin' }
+    { symbol: 'DOGE', name: 'Dogecoin', id: 'dogecoin' },
+    { symbol: 'SHIB', name: 'Shiba Inu', id: 'shiba-inu' },
+    { symbol: 'WIF', name: 'dogwifhat', id: 'dogwifcoin' },
+    { symbol: 'SUI', name: 'Sui', id: 'sui' },
+    { symbol: 'WOO', name: 'WOO', id: 'woo-network' },
+    { symbol: 'BCH', name: 'Bitcoin Cash', id: 'bitcoin-cash' },
 ];
