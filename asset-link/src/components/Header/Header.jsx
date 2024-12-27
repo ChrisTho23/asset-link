c2d43247ca7755e2eddd649154b4692c28b93dce
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-12-27 15:15:45 +0100

    Add user page

--- asset-link/src/components/Header/Header.jsx
+++ asset-link/src/components/Header/Header.jsx
@@ -1,25 +1,17 @@
 import { useNavigate, useLocation } from 'react-router-dom';
-import signOut from '../../pages/home/functions/signOut';
 import { getSession } from '../../pages/home/functions/sessionManager';
+import { icons } from '../../assets/icons';
 import './Header.css';
 
 const Header = () => {
     const navigate = useNavigate();
     const location = useLocation();
-    const isOverviewPage = location.pathname.includes('/overview');
+    const session = getSession();
 
     const handleLogoClick = () => {
-        const session = getSession();
         if (session?.user) {
             navigate(`/overview/${session.user.id}`);
         } else {
-            navigate('/');
-        }
-    };
-
-    const handleSignOut = async () => {
-        const result = await signOut();
-        if (result.success) {
             navigate('/');
         }
     };
@@ -31,9 +23,14 @@ const Header = () => {
                 <h2 className="header-title">Asset Link</h2>
             </div>
             <div className="header-right">
-                {isOverviewPage && (
-                    <button className="sign-out-button" onClick={handleSignOut}>
-                        Sign Out
+                {session?.user && (
+                    <button
+                        className="user-button"
+                        onClick={() => navigate(`/user/${session.user.id}`)}
+                    >
+                        <span className="user-icon">
+                            {icons.find(i => i.id === 'user-icon').icon}
+                        </span>
                     </button>
                 )}
                 <button className="about-button" onClick={() => navigate('/about')}>About</button>
--- asset-link/src/index.jsx
+++ asset-link/src/index.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home.jsx';
 import About from './pages/about/about.jsx';
 import Layout from './pages/layout/layout.jsx';
 import Overview from './pages/overview/overview.jsx';
 import EmailConfirmation from './pages/confirm-email/emailConfirmation.jsx';
+import User from './pages/user/User.jsx';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='about' element={<About />} />
           <Route path='confirm-email' element={<EmailConfirmation />} />
           <Route path='overview/:id' element={<Overview />} />
+          <Route path='user/:id' element={<User />} />
         </Route>
       </Routes>
     </Router>
